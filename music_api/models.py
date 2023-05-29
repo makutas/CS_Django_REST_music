3424464e0cc243e656050e0d5fbd90363728d777
Andrius Makutas <andrius.makutas@gmail.com>
2023-05-29 19:56:28 +0300

    add views, urls

--- music_api/models.py
+++ music_api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Band(models.Model):
@@ -13,7 +14,7 @@ class Album(models.Model):
     album_id = models.AutoField(primary_key=True)
     band = models.ForeignKey(Band, on_delete=models.CASCADE, related_name='albums')
     name = models.CharField(max_length=100)
-    year = models.IntegerField()
+    year = models.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(9999)])
 
     def __str__(self):
         return self.name
