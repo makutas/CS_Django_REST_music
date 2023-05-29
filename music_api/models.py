2bf30a49e890476bbf439f49634e95fdabee2bbf
Andrius Makutas <andrius.makutas@gmail.com>
2023-05-29 19:41:12 +0300

    add models.py

--- music_api/models.py
+++ music_api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Band(models.Model):
+    band_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    album_id = models.AutoField(primary_key=True)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE, related_name='albums')
+    name = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    song_id = models.AutoField(primary_key=True)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='songs')
+    name = models.CharField(max_length=100)
+    duration = models.FloatField()
+
+    def __str__(self):
+        return f'{self.name} - {self.duration}'
